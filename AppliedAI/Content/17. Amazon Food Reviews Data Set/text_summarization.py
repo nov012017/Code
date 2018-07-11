e264b5382d0c3f088173fbb19e6705bc7471d989
nov012017 <34241361+nov012017@users.noreply.github.com>
2018-07-11 08:31:04 +0530

    July 11

--- AppliedAI/Content/17. Amazon Food Reviews Data Set/text_summarization.py
+++ AppliedAI/Content/17. Amazon Food Reviews Data Set/text_summarization.py
@@ -7,10 +7,27 @@ Created on Tue Jul 10 10:19:24 2018
 
 import bs4 as bs
 import urllib.request
+import re
+import nltk
+nltk.download('stopwords')
 
-source=urllib.request.urlopen('https://en.wikipedia.org/wiki/Global_warming')
+source=urllib.request.urlopen('https://en.wikipedia.org/wiki/Global_warming').read()
 
 soup=bs.BeautifulSoup(source,'lxml')
 
-text
 
+text=""
+for paragraph in soup.find_all('p'):
+    #print(paragraph.text)
+    text+=paragraph.text
+    print(text)
+text=re.sub(r"\[[0-9]*\]"," ",text)
+text=re.sub(r'\s+',' ',text)
+clean_text=text.lower()
+clean_text=re.sub(r"\W"," ",clean_text) #special characters
+clean_text=re.sub(r"\d"," ",clean_text)
+clean_text=re.sub(r'\s+',' ', clean_text)
+
+sentences=nltk.sent_tokenize(text)
+
+stopwords=nltk.corpus.stopwords.words('english')
