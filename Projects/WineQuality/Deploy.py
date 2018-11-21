724c73f1a5e3b8db1a96ca205934736e62129b0c
nov012017 <34241361+nov012017@users.noreply.github.com>
2018-11-21 21:11:26 +0530

    Nov 21 Evening

--- Projects/WineQuality/Deploy.py
+++ Projects/WineQuality/Deploy.py
@@ -6,6 +6,7 @@ Created on Tue Nov 20 22:05:19 2018
 """
 
 import numpy as np
+import pandas as pd
 from flask import Flask,abort,jsonify,request
 import _pickle as pickle
 
@@ -16,16 +17,22 @@ example_dict = pickle.load(pickle_in)
 app=Flask(__name__)
 @app.route('/api',methods=['POST'])
 def make_predict():
+	data=request.get_json(force=True)
     ## all kinds of error checking should go here
-    data=request.get_json(force=True)
+	#dataDF=pd.DataFrame(json_)
     ## Convert our json to a numpy array
-    predict_request=[data['fixed acidity'],data['volatile acidity'],data['citric acid'],data['residual sugar'],data['chlorides'],data['	free sulfur dioxide'],data['total sulfur dioxide'],data['density'],data['pH'],data['sulphates'],data['alcohol']]
-    predict_request=np.array(predict_request)
+	predict_request=[data['fixed acidity'],data['volatile acidity'],data['citric acid'],data['residual sugar'],data['chlorides'],data['free sulfur dioxide'],data['total sulfur dioxide'],data['density'],data['pH'],data['sulphates'],data['alcohol']]
+	print(predict_request)
+	predict_request=np.array(predict_request).reshape(1,-1)
+	
+	#print(dataDF)
     ##np array goes into random forest, prediction comes out
-    y_hat=example_dict.predict(predict_request)
+	y_hat=example_dict.predict(predict_request)
+	print(y_hat)
     ## return our prediction
-    output=[y_hat[0]]
-    return jsonify(results=output)
+	output=[y_hat[0]]
+	return jsonify(results=output)
+	#return y_hat
 
 if __name__ == '__main__':
     app.run(debug=True, port=9000)
